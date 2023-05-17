c7c3a6075ee6b094da3dc9574e36d1bc14986b01
Aravindhthamizh0203 <aravindhthamizh0203@gmail.com>
2023-05-17 15:41:40 +0530

    clear

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,7 +8,7 @@ function Home() {
                 <div className='container'>
                     <div className='row'>
                         <div className='col-md-6 img'><img src={avti} alt='avti logo' className='avatar' /></div>
-                        <div className='col-md-6'><div className='text'><h1>Hi,I am ARAVINDH</h1>  </div>
+                        <div className='col-md-5'><div className='text'><h1>Hi,I am ARAVINDH</h1>  </div>
                             <div className='cont'>Highly motivated and enthusiastic Full Stack Developer with experience in designing, developing and maintaining web applications using technologies such as JavaScript, React, Node.js, Express-js and databases such as SQL MongoDB.</div>
                         </div>
                     </div>
